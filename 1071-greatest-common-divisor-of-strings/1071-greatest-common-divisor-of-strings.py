80c267b636384de2889ac299422675097794cdfe
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-07-03 21:49:03 +0530

    Time: 39 ms (41.29%), Space: 16.4 MB (98.07%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -1,14 +1,17 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        if str1+ str2 != str2+str1:
+        if str1 + str2 != str2 + str1:
             return ''
-        
-        if len(str1) == len(str2):
+        if str1 == str2:
             return str1
         
-        
         if len(str1) > len(str2):
-            return self.gcdOfStrings(str1[len(str2):], str2)
+            l = str1
+            s = str2
         else:
-            return self.gcdOfStrings(str1, str2[len(str1):])
+            l = str2
+            s = str1
+            
+        return self.gcdOfStrings(l[len(s):],s)
+
             
