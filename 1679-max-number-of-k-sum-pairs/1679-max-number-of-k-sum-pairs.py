d7b73a8d9a17649ed920c7abce8473043d988955
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-07-05 18:22:24 +0530

    Time: 1166 ms (5.01%), Space: 28.9 MB (41.56%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,6 +1,38 @@
+def merge(l,r):
+    i = 0
+    j = 0
+    s_l = []
+    while i < len(l) and j < len(r):
+        if l[i]<r[j]:
+            s_l.append(l[i])
+            i += 1
+        else:
+            s_l.append(r[j])
+            j += 1
+    
+    while i < len(l):
+        s_l.append(l[i])
+        i += 1
+    while j < len(r):
+        s_l.append(r[j])
+        j += 1
+        
+    return s_l
+
+def merSort(arr,l,r):
+    if r - l <= 1:
+        return arr[l:r]
+    
+    m = (r + l) // 2
+    L = merSort(arr,l,m)
+    R = merSort(arr,m,r)
+    
+    return merge(L,R)
+    
+            
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        n = sorted(nums)
+        n = merSort(nums,0,len(nums))
         l = 0
         r = len(nums) - 1
         ops = 0
