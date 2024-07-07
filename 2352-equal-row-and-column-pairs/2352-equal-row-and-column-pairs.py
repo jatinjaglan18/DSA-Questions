14c41cca5b4448989d8624935a7e2dcfd3bb24ab
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-07-07 12:32:12 +0530

    Time: 391 ms (63.66%), Space: 21.5 MB (42.54%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -3,18 +3,20 @@ class Solution:
         g1 = {}
         for i in grid:
             g1[tuple(i)] = 1 +g1.get(tuple(i),0)
-            
+        
+        count = 0 
         g2 = {}  
         for col in range(len(grid)):
             r = []
             for row in range(len(grid)):
                 r.append(grid[row][col])
             
-            g2[tuple(r)] = 1 + g2.get(tuple(r),0)
+            count += g1.get(tuple(r),0)
+            #g2[tuple(r)] = 1 + g2.get(tuple(r),0)
         
-        count = 0
-        for i in g1.keys():
+        
+        '''for i in g1.keys():
             if i in g2.keys():
-                count += g1[i] * g2[i]
+                count += g1[i] * g2[i]'''
                 
         return count
