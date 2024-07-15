8f033329fbda72f8f85bb62a65c4d19011af8c03
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-07-15 10:41:45 +0530

    Time: 692 ms (83.04%), Space: 27.5 MB (6.39%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        stack = [0]
+        '''stack = [0]
         nge = [len(prices)] * len(prices)
         for i in range(len(prices)):
             while prices[i] > prices[stack[-1]]:
@@ -12,7 +12,6 @@ class Solution:
             else:
                 stack.append(i)
         
-        print(nge)
         profit = 0
         for i in range(len(nge)):
             j = nge[i]
@@ -20,8 +19,17 @@ class Solution:
                 p = prices[j]-prices[i]
                 if p > profit:
                     profit = p    
-                j = nge[j]
-        print(profit)
+                j = nge[j]'''
+        
+        
+        profit = 0
+        buy = prices[0]
+        for i in prices:
+            if i < buy:
+                buy = i
+            elif i - buy > profit:
+                profit = i - buy
+                
             
         return profit
             
