d59c5108f53aba3fb2bc208419f4c6dfe9a4d52c
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-07-15 21:18:54 +0530

    Time: 84 ms (69.49%), Space: 16.5 MB (93.96%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,10 +3,11 @@ class Solution:
         d = {}
         l = 0
         max_len = 0
+        m_f = 0
         for i in range(len(s)):
             d[s[i]] = 1 + d.get(s[i],0)
-            
-            while (i - l + 1) - max(d.values()) > k:
+            m_f = max(m_f,d[s[i]])
+            while (i - l + 1) - m_f > k:
                 d[s[l]] -= 1
                 l += 1
                 
