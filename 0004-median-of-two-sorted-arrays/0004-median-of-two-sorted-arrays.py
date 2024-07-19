0673e3a7e550b3697a2cb91d0413dfa597f6e434
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-07-19 07:00:50 +0530

    Time: 83 ms (44.34%), Space: 17 MB (19.10%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,19 +1,20 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         nums = self.merge(nums1,nums2)
-        mid = len(nums) // 2
-        if len(nums) % 2 == 0: 
-            median = (nums[mid-1] + nums[mid]) / 2
-            return median
+        print(nums)
+        if (len(nums1) + len(nums2)) % 2 == 0:
+            return (nums[-1] + nums[-2]) / 2
         else:
-            return (nums[mid])
-        
+            return nums[-1]
     
     def merge(self,arr1,arr2):
         a1 = 0
         a2 = 0
+        mid = ((len(arr1) + len(arr2)) // 2)+1
         arr = []
         while a1 < len(arr1) and a2 < len(arr2):
+            if len(arr) == mid:
+                return arr
             if arr1[a1] < arr2[a2]:
                 arr.append(arr1[a1])
                 a1 += 1
@@ -22,11 +23,17 @@ class Solution:
                 a2 += 1
         
         while a1 < len(arr1):
+            if len(arr) == mid:
+                return arr
             arr.append(arr1[a1])
             a1 += 1
         
         while a2 < len(arr2):
+            if len(arr) == mid:
+                return arr
             arr.append(arr2[a2])
             a2 += 1
-    
+            
         return arr
+    
+        
