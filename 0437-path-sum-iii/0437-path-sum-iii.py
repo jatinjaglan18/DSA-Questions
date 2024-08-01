b5a658ce0526e403f4ff0762ad01ffad684cdafb
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-08-01 20:48:17 +0530

    Time: 54 ms (50.47%), Space: 17.1 MB (31.70%) - LeetHub

--- 0437-path-sum-iii/0437-path-sum-iii.py
+++ 0437-path-sum-iii/0437-path-sum-iii.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        self.sum = 0
+        '''self.sum = 0
         def helper(node,cur):
             if node == None:
                 return
@@ -22,7 +22,31 @@ class Solution:
             dfs(node.right)
             
         dfs(root)
+        return self.sum'''
+        
+        self.sum = 0
+        d = {0:1}
+        def dfs(node,cur):
+            if node == None:
+                return 
+            ans = (cur + node.val) - targetSum
+            if ans in d.keys() :
+                self.sum += d[ans]
+                
+            d[cur+node.val] = 1 + d.get(cur+node.val,0)
+            
+            dfs(node.left,cur+node.val)
+            dfs(node.right,cur+node.val)
+            
+            d[cur+node.val] -= 1
+            
+        dfs(root,0)
         return self.sum
+            
+            
+            
+            
+            
 
     
     
