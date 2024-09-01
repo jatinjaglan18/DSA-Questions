fc5a812bb3c4a9c9f8f0333728b8085a49bfdfcc
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-09-01 11:32:44 +0530

    Time: 638 ms (86.63%), Space: 17.2 MB (57.16%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.py
@@ -44,21 +44,21 @@ class Solution:
         m = len(maze)
         n = len(maze[0])
 
-        queue = deque([(entrance[0], entrance[1], 0)]) # step 1
-        maze[entrance[0]][entrance[1]] = "+" # step 2
+        queue = deque([(entrance[0], entrance[1], 0)])
+        maze[entrance[0]][entrance[1]] = "+"
 
         while queue:
-            row, col, steps = queue.popleft() # step 3
+            row, col, steps = queue.popleft()
 
-            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]: # step 4
+            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
                 nr, nc = row + dy, col + dx
-                if 0 <= nr < m and 0 <= nc < n and maze[nr][nc] == ".": # step 5
+                if 0 <= nr < m and 0 <= nc < n and maze[nr][nc] == ".":
                     if (nr == 0 or nr == m - 1) or (nc == 0 or nc == n - 1):
                         return steps + 1
 
                     maze[nr][nc] = "+"
                     queue.append((nr, nc, steps + 1))
 
-        return -1 # step 6
+        return -1
             
             
