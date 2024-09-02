e10b8e767f0e1f71d915a0561eeb437c06d4c1e0
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-09-02 10:14:47 +0530

    Time: 45 ms (84.88%), Space: 16.6 MB (58.89%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
-                    q.append([i,j])       #r,c,t
+                    q.append([i,j,0])       #r,c,t
                 if grid[i][j] == 1:
                     count += 1
                 
@@ -16,7 +16,7 @@ class Solution:
             return -1
         
         time = -1
-        while q:
+        '''while q:
             size = len(q)
             while size :
                 val = q.popleft()
@@ -30,8 +30,24 @@ class Solution:
                     if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 1:
                         grid[nr][nc] = 2
                         q.append([nr,nc])
-                        count -= 1
-            time += 1
+                        count -= 1'''
+        while q:
+            val = q.popleft()
+            r = val[0]
+            c = val[1]
+            t = val[2]
+            time = t
+            
+            
+            
+            for dr,dc in [(-1,0),(0,1),(1,0),(0,-1)]:
+                nr = r + dr
+                nc = c + dc
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and grid[nr][nc] == 1:
+                    grid[nr][nc] = 2
+                    q.append([nr,nc,t+1])
+                    count -= 1
+            
                     
         if count != 0:
             return -1
