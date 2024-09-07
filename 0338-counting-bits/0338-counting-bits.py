45ce9de87ebffaa70af1d8f4e7b4d151188de6aa
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-09-07 09:25:52 +0530

    Time: 58 ms (77.83%), Space: 23.3 MB (45.31%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,6 +1,6 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        ans = []
+        '''ans = []
         for i in range(n+1):
             count = 0
             while i > 0 :
@@ -8,4 +8,25 @@ class Solution:
                     count += 1
                 i = i // 2
             ans.append(count)
-        return ans
+        return ans'''
+        
+        if n<=1:
+            if n == 0 :
+                return [0]
+            else:
+                return [0,1]
+					
+        dp = [0]*(n+1)
+        dp[1] = 1
+        dp[2] = 1
+        for i in range(3,n+1):
+            if i%2==0:
+                val = i//2
+                dp[i] = dp[val]
+					# val its basically i<<2
+                    
+            else:
+                dp[i] = dp[i-1]+1
+                
+                
+        return dp
