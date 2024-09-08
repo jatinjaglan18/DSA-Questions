9b4db6d5d0c2399ae4407f71870f2a23031e998e
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-09-08 11:53:28 +0530

    Time: 110 ms (58.67%), Space: 19 MB (55.02%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d = {}
+        # Time - O(n)
+        # space - O(n)
+        '''d = {}
         for i in nums:
             d[i] = 1 + d.get(i,0)
             
         for i in d.keys():
             if d[i] == 1:
-                return i
+                return i'''
+        
+        # Time - O(n)
+        # space - O(n)
+        #X-OR gate
+        xr = 0
+        for i in nums:
+            xr ^= i  
+        return xr
