416bd45f3949143771996a1fc7cc25c880a8ce96
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-09-22 08:23:52 +0530

    Time: 42 ms (17.37%), Space: 16.6 MB (29.00%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -4,29 +4,24 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-from collections import deque
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if root == None:
+        if not root:
             return []
-        
-        arr = []
-        mq = deque()
-        cq = deque()
-        mq.append(root)
-        
-        while len(mq) != 0 or len(cq) != 0:
-            v = mq.popleft()
-            if v.left != None:
-                cq.append(v.left)
-            if v.right != None:
-                cq.append(v.right)
+        q = [root]
+        cq = []
+        ans = []
+        while q:
+            val = q.pop(0)
+            if val.left:
+                cq.append(val.left)
+            if val.right:
+                cq.append(val.right)
             
-            if len(mq) == 0:
-                arr.append(v.val)
-                mq = cq
-                cq = deque()
-        
-        return arr
-        
-        
+            if len(q) == 0:
+                ans.append(val.val)
+                q = cq
+                cq = []
+                
+ 
+        return ans
