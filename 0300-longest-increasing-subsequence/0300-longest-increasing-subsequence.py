a5aac36ba927a648a619aed7200d543451e4ca91
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-11-04 09:14:10 +0530

    Time: 3 ms (96.08%), Space: 17 MB (19.04%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,8 +1,9 @@
+import bisect
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         res = []
         
-        def binarySearch(res, n):
+        '''def binarySearch(res, n):
             left = 0
             right = len(res) - 1
             
@@ -15,14 +16,16 @@ class Solution:
                 else:
                     left = mid + 1
                 
-            return left
+            return left'''
         
         for i in nums:
             if len(res) == 0 or res[-1] < i:
                 res.append(i)
             else:
-                idx = binarySearch(res,i)
-                res[idx] = i
+                '''idx = binarySearch(res,i)
+                res[idx] = i'''
+                
+                res[bisect.bisect_left(res,i)] = i
         
         return len(res)
                     
