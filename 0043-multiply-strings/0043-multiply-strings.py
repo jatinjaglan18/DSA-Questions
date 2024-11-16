9d14a4f59966ae8b5ab1a0950c03a548f625e984
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-11-17 01:13:51 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (92.71%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,6 +1,6 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        a=b=0
+        '''a=b=0
         for i in num1:
             a = a*10+(ord(i) - 48)
         for i in num2:
@@ -8,7 +8,8 @@ class Solution:
         
         return str(a*b)
         
-        '''num1 = int(num1)
+        '''
+        num1 = int(num1)
         num2 = int(num2)
         
-        return str(num1*num2)'''
+        return str(num1*num2)
