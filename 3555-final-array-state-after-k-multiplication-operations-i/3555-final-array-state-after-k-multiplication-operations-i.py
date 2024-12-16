83715ee493d03e36a840d1c608194e2f8f73a03b
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-12-16 09:07:41 +0530

    Time: 3 ms (52.21%), Space: 17.3 MB (28.27%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.py
@@ -1,16 +1,8 @@
 import heapq
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        n = []
-        for i in range(len(nums)):
-            n.append([nums[i], i])
-        #print(n)
-        heapq.heapify(n)
         for i in range(k):
-            val=heapq.heappop(n)
-            ele = val[0] * multiplier
-            nums[val[1]] = ele
-            heapq.heappush(n,[ele, val[1]])
-        return nums
-        
+            idx = nums.index(min(nums))
+            nums[idx] *= multiplier
+        return nums   
         
