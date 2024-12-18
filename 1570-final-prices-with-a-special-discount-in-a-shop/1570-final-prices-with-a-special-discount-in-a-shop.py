236ef89ca3dc41471bb8996435c4c4861e4e0a43
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-12-18 08:14:34 +0530

    Time: 3 ms (55.13%), Space: 17.5 MB (10.22%) - LeetHub

---
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,12 @@
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        for i in range(len(prices)-1):
+            j = i+1
+            while j < len(prices) and prices[j] > prices[i]:
+                j += 1
+                if j == len(prices):
+                    break
+            else:
+                prices[i] -= prices[j]
+        return prices
+                
