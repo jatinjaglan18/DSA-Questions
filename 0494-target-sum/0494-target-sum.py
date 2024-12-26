86bbb9b562fe582e41d8d7f0e2633f8504fbbfe1
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2024-12-26 11:39:45 +0530

    Time: 39 ms (90.93%), Space: 18.3 MB (59.59%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,27 +1,5 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        
-        '''memo = {}
-        
-        def backtrack(i, total):
-            if i == len(nums):
-                if total == target:
-                    return 1
-                else: 
-                    return 0
-            
-            if (i,total) in memo:
-                return memo[(i,total)]
-            
-            add = backtrack(i+1, total + nums[i])
-            subtract = backtrack(i+1, total - nums[i])
-            
-            memo[(i,total)] = add + subtract
-            
-            return memo[(i,total)]
-        
-        return backtrack(0,0)'''
-        
         if (sum(nums) + target) % 2 != 0 or sum(nums) + target < 0:
             return 0
 
@@ -29,9 +7,10 @@ class Solution:
         
         dp = [0] * (subset_sum + 1)
         dp[0] = 1
-        
+        print(subset_sum)
         for num in nums:
             for i in range(subset_sum, num - 1, -1):
                 dp[i] += dp[i - num]
+            print(dp)
         return dp[subset_sum]
-                
+        
