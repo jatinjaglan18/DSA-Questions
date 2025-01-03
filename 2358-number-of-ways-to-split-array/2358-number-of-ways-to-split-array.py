5d84e389cf9a2d7df50a6612169c11b03c4af732
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2025-01-03 07:36:23 +0530

    Time: 58 ms (61.47%), Space: 32.2 MB (63.7%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.py
@@ -1,7 +1,7 @@
 class Solution:
     def waysToSplitArray(self, nums: List[int]) -> int:
         n = len(nums)
-        left_sum = [0] * n
+        '''left_sum = [0] * n
         right_sum = [0] * n
         left_sum[0] = nums[0]
         right_sum[-1] = nums[-1]
@@ -15,4 +15,14 @@ class Solution:
             if left_sum[i] >= right_sum[i+1]:
                 ans+=1
 
+        return ans'''
+
+        left_sum = 0
+        total_sum = sum(nums)
+        ans = 0
+        for i in range(n-1):
+            left_sum += nums[i]
+            right_sum = total_sum - left_sum
+            if left_sum >= right_sum:
+                ans+=1
         return ans
