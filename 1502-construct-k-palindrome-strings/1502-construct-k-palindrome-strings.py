b94adedf781480e4b0e0c5d91c65d4baa37ed77f
jatinjaglan18 <141440873+jatinjaglan18@users.noreply.github.com>
2025-01-11 09:38:39 +0530

    Time: 50 ms (26.43%), Space: 18.1 MB (16.3%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -1,7 +1,5 @@
 class Solution:
     def canConstruct(self, s: str, k: int) -> bool:
-        if k == len(s):
-            return True
         if len(s) < k:
             return False
         
@@ -12,7 +10,5 @@ class Solution:
         for i in freq.keys():
             if freq[i] % 2 != 0:
                 count += 1
-        if count > k:
-            return False
-        return True
+        return count <= k
         
